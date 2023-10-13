4a5471bf0bb62f82f51d32c1c93ec857012829cb
Abraham Cordero <abrahamicm2@gmail.com>
2023-10-13 08:21:10 -0400

    .

--- components/voice-player.js
+++ components/voice-player.js
@@ -4,39 +4,45 @@ export class VoicePlayer extends HTMLElement {
       super();
       this.speechSynthesis = window.speechSynthesis;
       this.playing = false;
-      
+      this.errorMessage = '';
+
       this.shadow = this.attachShadow({ mode: 'open' });
       this.shadow.innerHTML = `
           <style>
               button {
                   cursor: pointer;
               }
+              .error {
+                  color: red;
+              }
           </style>
           <button id="playPause">Play</button>
           <button id="stop">Stop</button>
+          <span class="error"></span>
       `;
 
       this.playPauseButton = this.shadow.getElementById('playPause');
       this.stopButton = this.shadow.getElementById('stop');
+      this.errorSpan = this.shadow.querySelector('.error');
 
       this.playPauseButton.addEventListener('click', () => this.togglePlayPause());
       this.stopButton.addEventListener('click', () => this.stopSpeech());
   }
 
-  connectedCallback() {
-      const textSelector = this.getAttribute('text-selector');
-      if (textSelector) {
-          this.textToRead = document.querySelector(textSelector).textContent;
-      } else {
-          this.textToRead = 'Texto de ejemplo';
-      }
+  showError(message) {
+      this.errorSpan.textContent = message;
+  }
+
+  clearError() {
+      this.errorSpan.textContent = '';
   }
 
   togglePlayPause() {
+      this.clearError(); // Limpiar el mensaje de error antes de intentar reproducir
       if (this.playing) {
           this.pauseSpeech();
       } else {
-          this.playSpeech(this.textToRead);
+          this.playSpeech('Hola, este es un ejemplo de síntesis de voz.');
       }
   }
 
@@ -46,10 +52,16 @@ export class VoicePlayer extends HTMLElement {
           this.playPauseButton.textContent = 'Pause';
           this.utterance = new SpeechSynthesisUtterance(text);
           this.speechSynthesis.speak(this.utterance);
-          
+
           this.utterance.onend = () => {
               this.playing = false;
               this.playPauseButton.textContent = 'Play';
+          };
+
+          this.utterance.onerror = (event) => {
+              this.playing = false;
+              this.playPauseButton.textContent = 'Play';
+              this.showError('Error en la síntesis de voz: ' + event.error);
           };
       }
   }
